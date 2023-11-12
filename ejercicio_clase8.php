c17bf309b3d6f0adbfb9f764b90b96796950e416
alesiapando <138126244+alesiapando@users.noreply.github.com>
2023-11-12 14:58:41 -0300

    Add files via upload

---
+++ ejercicio_clase8.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ejercicio Clase 8</h1><br>
+
+    <h2>Mediante código PHP, consultar a la base de datos lo siguiente: </h2><br>
+    <h2>1 - Listar todos los registros: </h2><br>
+    <p><a href="toda_la_ropa.php">En este link encontrarás todos los artículos en Stock</p> 
+    <br>
+    <br>
+    <h2>2 - Listar todos los registros cuya marca sea nike </h2><br>
+    <p><a href="nike.php">En este link encontrarás todos los artículos de la Marca Nike</p> 
+    <br>
+    <br>
+    <h2>3 - Listar todos los registros cuyo precio sea mayor a 500 </h2><br>
+    <p><a href="mayor_a_500.php">En este link encontrarás todos los artículos cuyo valor es mayor a 500</p> 
+
+ 
+
+</body>
+</html>
